3e1d731ab0c0fd8795c05b208e30c403a1ca2f1c
MohdShanb-10 <itz.unknown.baby@gmail.com>
2026-04-07 23:14:25 +0530

    initial commit

--- list element shifting.py
+++ list element shifting.py
@@ -1,6 +1,6 @@
 a=[10,23,25,56,12,34]
 print(a)
-print('This program shift the elements of the list to the next place. ')
+print('This program shifts the elements of the list to the next place. ')
 n=len(a)
 l=a[-1]
 for i in range(n-1,0,-1):
