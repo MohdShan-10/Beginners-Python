e9416c3635406b28cb6c1d2c08243aa04dd3f0a9
MohdShan-10 <itz.unknown.baby@gamil.com>
2026-04-07 21:29:10 +0530

    initial commit

---
+++ BMI.py
@@ -0,0 +1,14 @@
+print('This Program calculates your BMI (Body Mass Index)')
+weight = float(input('Enter your weight in kg ==> '))
+height = float(input('Enter your height in meters ==> '))
+bmi = weight / (height ** 2)
+print('Your BMI is ==> ', bmi)
+if bmi < 18.5:
+    print('You are underweight')
+elif bmi >= 18.5 and bmi < 25:
+    print('You are normal weight')
+elif bmi >= 25 and bmi < 30:
+    print('You are overweight')
+else:
+    print('You are obese')
+print('Thank You! ')
